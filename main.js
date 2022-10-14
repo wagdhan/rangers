88445f464496ecd58e548f364168844911738d62
wagdhan <99190652+wagdhan@users.noreply.github.com>
2022-10-14 18:04:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+
+// Create canvas variable
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
+{
+	// to upload images
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '?') // add appropriate keycode
+	{
+		// upload red ranger
+	}
+	if(keyPressed == '71')
+	{
+		block_x = 200;
+		// upload green ranger
+	}
+	
+	if(keyPressed == '89')
+	{
+		block_x =350;
+		// upload yellow ranger
+	}
+	if(keyPressed == '80')
+	{
+		block_x = 600;
+		// upload pink ranger
+	}
+	if(keyPressed == '66')
+	{
+		block_x = 700;
+	// upload blue ranger
+	}
+	
+}
+fabric.image.fromURL('Birthdayimage.jpg',function(Img))
+block_image_object=Img;
+block_image_object.scaleTowidth(700);
+block_image_object.scaleToHeight(510);
+}block_image_object.set({
+top:0
+left:0
+});
+canvas.add( block_image_object);
+})
